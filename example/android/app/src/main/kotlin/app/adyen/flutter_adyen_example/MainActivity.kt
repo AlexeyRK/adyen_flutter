08ab77a9b162e2344324705a6c802be80c68e6f6
Alexey RK <forcscart@gmail.com>
2023-08-07 09:48:27 +0400

    android fixes

--- example/android/app/src/main/kotlin/app/adyen/flutter_adyen_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/app/adyen/flutter_adyen_example/MainActivity.kt
@@ -1,14 +1,8 @@
 package app.adyen.flutter_adyen_example
 
 
-import android.content.Intent
-import app.adyen.flutter_adyen.AdyenDropinService
-import app.adyen.flutter_adyen.FlutterAdyenPlugin
-import io.flutter.Log
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
-    // You can keep this empty class or remove it. Plugins on the new embedding
-    // now automatically registers plugins.
-    // Note: This class is kept to make the project more realistic.
+
 }
